ee77fee8f951de406e28a3fb2dd01ce83bbd8760
QuixoticZ <zwubu@NV-001.>
2025-02-09 16:34:10 -0500

    update with cirlceshape

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
@@ -18,6 +18,9 @@ def main():
 
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
